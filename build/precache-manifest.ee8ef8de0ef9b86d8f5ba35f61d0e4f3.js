2c32c104a62a6206895d4d06d46c0901da7b2d78
Helena Makendengue <HelenaMakens1013@gmail.com>
2019-10-04 12:48:58 -0400

    deployment test

---
+++ build/precache-manifest.ee8ef8de0ef9b86d8f5ba35f61d0e4f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b9af2e8edf883e5f318c3bf469182f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13e58cd2842be7c3a2c1",
+    "url": "/static/css/main.bf08bdc1.chunk.css"
+  },
+  {
+    "revision": "8f6c866c902d4f9e9abc",
+    "url": "/static/js/2.721ca802.chunk.js"
+  },
+  {
+    "revision": "13e58cd2842be7c3a2c1",
+    "url": "/static/js/main.ae6cdab3.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  },
+  {
+    "revision": "37cef04c5666410c62f64a452d9a86d4",
+    "url": "/static/media/about1.37cef04c.jpg"
+  },
+  {
+    "revision": "141623b5b965922363cf785bfee78e58",
+    "url": "/static/media/contact1.141623b5.jpeg"
+  }
+]);
