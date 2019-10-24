11a6012ce2d76948c2fcdcfb737a9861051f0e73
Helena Makendengue <HelenaMakens1013@gmail.com>
2019-10-24 00:46:14 -0400

    Updates

---
+++ precache-manifest.8bc3d0a5ded04510722eefc6b3e5b00e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fcd6545d3fd1586f541fc9a8f328b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "030b9217d8b31b26ff53",
+    "url": "/static/css/main.2ab40956.chunk.css"
+  },
+  {
+    "revision": "15a20417ea650f400433",
+    "url": "/static/js/2.334a094f.chunk.js"
+  },
+  {
+    "revision": "030b9217d8b31b26ff53",
+    "url": "/static/js/main.f9d82d44.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  },
+  {
+    "revision": "cb209d7bb92143b426fa447f8bd4a153",
+    "url": "/static/media/header1.cb209d7b.jpg"
+  },
+  {
+    "revision": "a815d6711d0562b0fd4171cf4969630d",
+    "url": "/static/media/helena.a815d671.jpeg"
+  }
+]);
