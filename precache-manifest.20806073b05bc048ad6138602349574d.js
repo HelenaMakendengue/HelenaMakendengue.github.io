1d07c515a7400398b54b221a461939051e3d04ff
Helena Makendengue <HelenaMakens1013@gmail.com>
2020-04-26 11:12:51 -0400

    Updates

---
+++ precache-manifest.20806073b05bc048ad6138602349574d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2537e3e6659e7fc3b577c64a70fc3dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8cb9bed49a4654b6c0",
+    "url": "/static/css/main.dadbd67d.chunk.css"
+  },
+  {
+    "revision": "223cabe22e1174ccd29c",
+    "url": "/static/js/2.80ea5eaf.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.80ea5eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8cb9bed49a4654b6c0",
+    "url": "/static/js/main.4f2be7d2.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "7f2f132ade11c771a622a454889f4618",
+    "url": "/static/media/beavis2.7f2f132a.gif"
+  },
+  {
+    "revision": "d8925183e2315cc792a4b0c6f3d8864b",
+    "url": "/static/media/helena3.d8925183.jpeg"
+  },
+  {
+    "revision": "45a2587e267661111ee6fac5a96b4472",
+    "url": "/static/media/resume.45a2587e.pdf"
+  }
+]);
